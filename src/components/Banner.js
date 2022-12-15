3b72f0e1e56ae9e9395f261cbd2af7fcaa0f03e7
yang <joey674901205@gmail.com>
2022-12-15 15:43:21 -0500

    update info

---
+++ src/components/Banner.js
@@ -0,0 +1,104 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my portfolio</span>
+                  <div border="light" style={{ height: "150px" }}>
+                    <h1>
+                      {`Hi I'm Longyang Wang `}
+                      <span className="wrap">{text}</span>
+                    </h1>
+                  </div>
+                  <p>
+                    I'm a full-stack developer. Emerging full-stack web
+                    developer who is certificate in Full Stack Web Development
+                    from the University of Toronto Bootcamp with a strong
+                    interest in computer technology and a focus on better user
+                    experience on the web. Learned new skills in CSS,
+                    JavaScript, python, node.js, and responsive web design from
+                    Bootcamp. Be called an innovative practitioner and problem
+                    solver with a passion for developing apps, focusing on
+                    mobile-first design and development, with a clean and
+                    comfortable front-end UI. Each project can achieve the best
+                    readability, and the data structure is clear. Good
+                    understanding of project requirements and outlines. I have
+                    strong execution and coordination skills in teamwork and can
+                    quickly promote the plan's development. My goal is to be an
+                    excellent web developer. I like to face different challenges
+                    in the process, and I feel a strong sense of accomplishment
+                    when I finish the project and solve the problems encountered
+                    in programming. I'm excited to use my skills and
+                    characteristics to build better experiences on the web.
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    Let's connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/Projects.js
@@ -0,0 +1,123 @@
+import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+// import projImg4 from "../assets/img/project-img4.png";
+// import projImg5 from "../assets/img/project-img5.png";
+// import projImg6 from "../assets/img/project-img6.png";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const projects = [
+    {
+      name: "iBetter - Habits Tracker",
+      description:
+        "iBetter was developed to help you be better at tracking and accomplishing your good habits.",
+      language: "HANDLEBARS/JAVASCRIPT/CSS",
+      github: "https://github.com/Vinyldude8896/iBetter",
+      deployment: "https://ibetter.herokuapp.com",
+      imgUrl: projImg1,
+    },
+    {
+      name: "Movie Search",
+      description: "A movie search website.",
+      language: "HTML/JAVASCRIPT/BULMA/CSS",
+      github: "https://github.com/EnzoTragnone/Movie-Search",
+      deployment: "https://enzotragnone.github.io/Movie-Search/",
+      imgUrl: projImg2,
+    },
+    {
+      name: "The Tech Blog",
+      description:
+        "A CMS-style blog site so that people can publish articles, blog posts, and any thoughts and opinions.",
+      language: "HANDLEBARS/JAVASCRIPT/CSS",
+      github: "https://github.com/YangLongWang/Tech-Blog",
+      deployment: "https://tech-blog-app-6408.herokuapp.com",
+      imgUrl: projImg3,
+    },
+    {
+      name: "Text Editor",
+      description:
+        "To create notes or code snippets with or without an internet connection.",
+      language: "JAVASCRIPT",
+      github: "https://github.com/YangLongWang/Text-Editor",
+      deployment: "https://text-editor-app-6408.herokuapp.com/",
+      // imgUrl: projImg4,
+    },
+    {
+      name: "Weather Dashboard",
+      description:
+        "Seeing the weather outlook for multiple cities, travelers can plan a trip accordingly",
+      language: "HTML/JAVASCRIPT/CSS",
+      github: "https://github.com/YangLongWang/Weather-Dashboard",
+      deployment: "https://yanglongwang.github.io/Weather-Dashboard/",
+      // imgUrl: projImg5,
+    },
+    {
+      name: "Coding Quiz",
+      description: "Practice coding assignment",
+      language: "HTML/JAVASCRIPT/CSS",
+      github: "https://github.com/YangLongWang/Coding-Quiz",
+      deployment: "https://yanglongwang.github.io/Coding-Quiz/",
+      // imgUrl: projImg6,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__bounce" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s.
+                  </p>
+                </div>
+              )}
+            </TrackVisibility>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">Loren Ipsum</Tab.Pane>
+                <Tab.Pane eventKey="third">Loren Ipsum</Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2} alt="" />
+    </section>
+  );
+};
